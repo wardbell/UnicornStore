67afc8d124636e1e0d862a92ac9f60fba9e9cea8
Ward Bell <wardbell@hotmail.com>
2015-05-17 11:18:50 -0700

    :runner: ProductsController offers full CRUD

--- CompletedSource/UnicornStore/src/UnicornStore/Controllers/ProductsController.cs
+++ CompletedSource/UnicornStore/src/UnicornStore/Controllers/ProductsController.cs
@@ -5,6 +5,7 @@ using UnicornStore.AspNet.Models.UnicornStore;
 
 namespace UnicornStore.AspNet.Controllers
 {
+    //Todo: move all Entity Framework interaction to a repository/unit-of-work
 
     [Route("api/[controller]")]
     public class ProductsController : Controller
@@ -27,6 +28,18 @@ namespace UnicornStore.AspNet.Controllers
         {
             return db.Products;
         }
+
+        [HttpGet("{id:int}", Name = "ProductsGetByIdRoute")]
+        public IActionResult GetById(int id)
+        {
+            var product = db.Products.FirstOrDefault(c => c.ProductId == id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return new ObjectResult(product);
+        }
+
         [Route("First3Products")]
         public IEnumerable<Product> First3Products()
         {
@@ -40,5 +53,79 @@ namespace UnicornStore.AspNet.Controllers
                 .Where(p => p.CategoryId == id)
                 .Include(p => p.Category);
         }
+
+
+        [HttpPost]
+        [Route("")]
+        //[Authorize] // A MUST ... but leaving out for demo purposes
+        //[ValidateAntiForgeryToken] // Todo: support these tokens to prevent XSRF
+        public IActionResult CreateProduct([FromBody] Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return HttpBadRequest();
+            }
+            else
+            {
+                db.Products.Add(product);
+                db.SaveChanges();
+
+                string url = Url.RouteUrl("ProductsGetByIdRoute", new { id = product.ProductId },
+                    Request.Scheme, Request.Host.ToUriComponent());
+
+                Context.Response.StatusCode = 201;
+                Context.Response.Headers["Location"] = url;
+                return new ObjectResult(product);
+            }
+        }
+
+        [HttpPut]
+        [Route("")]
+        //[Authorize] // A MUST ... but leaving out for demo purposes
+        //[ValidateAntiForgeryToken]
+        public IActionResult UpdateProduct([FromBody] Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return HttpBadRequest();
+            }
+
+            var origProduct = db.Products.AsNoTracking().FirstOrDefault(x => x.ProductId == product.ProductId);
+            if (origProduct == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                // Todo: validation ... referencing the original product as necessary
+                db.Products.Update(product);
+                db.SaveChanges();
+                return new ObjectResult(product);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        [Route("{id}")]
+        //[Authorize] // A MUST ... but leaving out for demo purposes
+        //[ValidateAntiForgeryToken]
+        public IActionResult DeleteProduct(int id)
+        {
+            if (id <= MAX_ORIGINAL_ID)
+            {
+                Context.Response.StatusCode = 400;
+                return new ObjectResult("You may not delete one of the original UnicornStore products");
+            }
+
+            var product = db.Products.FirstOrDefault(x => x.ProductId == id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            db.Products.Remove(product);
+            db.SaveChanges();
+            return new HttpStatusCodeResult(204); // No Content
+        }
+
+        private const int MAX_ORIGINAL_ID = 7;
     }
 }
