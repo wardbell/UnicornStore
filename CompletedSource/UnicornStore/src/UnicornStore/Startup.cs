ba8860e53f915e26ce10ea7e3b8279766835640e
Ward Bell <wardbell@hotmail.com>
2015-05-18 20:01:56 -0700

    :wrench: revise default MVC route spec.

--- CompletedSource/UnicornStore/src/UnicornStore/Startup.cs
+++ CompletedSource/UnicornStore/src/UnicornStore/Startup.cs
@@ -140,8 +140,7 @@ namespace UnicornStore
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller}/{action}/{id?}",
-                    defaults: new { controller = "Home", action = "Index" });
+                    template: "{controller=Home}/{action=Index}/{id?}");
 
                 // Uncomment the following line to add a route for porting Web API 2 controllers.
                 // routes.MapWebApiRoute("DefaultApi", "api/{controller}/{id?}");
